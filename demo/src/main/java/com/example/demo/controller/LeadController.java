317eb0401481bb3a7c6f30010fccb900ac3903ff
Gustaf Faivre <ggwf01@gmail.com>
2025-05-14 00:51:18 +0200

    added dynamic lead functionality - 10 000

--- demo/src/main/java/com/example/demo/controller/LeadController.java
+++ demo/src/main/java/com/example/demo/controller/LeadController.java
@@ -20,33 +20,35 @@ public class LeadController {
 
     @GetMapping(value = "/api/leads", produces = MediaType.APPLICATION_XML_VALUE)
     public String getLeadsAsXml(@RequestParam(required = false, defaultValue = "3") Integer count) {
-        // Validera count för att undvika negativa värden eller för stora värden
+        // Validera count
         if (count < 0) {
-            count = 3; // Default om felaktigt värde
+            count = 3;
         }
         if (count > 10000) {
-            count = 10000; // Max 10 000 leads
+            count = 10000;
         }
 
         List<Lead> leads = leadRepository.findAll(count);
 
         StringBuilder xml = new StringBuilder();
         xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-        // Ändra namespace till det som förväntas av klienten
-        xml.append("<leads xmlns=\"http://webscraper.se.leads-format.1.0\">\n");
+        // Huvudnamespace för leads-taggen
+        xml.append("<leads xmlns=\"http://webscraper.se.leads-format.1.0\" xmlns:syn=\"http://ws.apache.org/ns/synapse\">\n");
 
         for (Lead lead : leads) {
-            xml.append("<lead id=\"").append(lead.getId()).append("\">\n");
-            xml.append(" <name>").append(lead.getName()).append("</name>\n");
-            xml.append(" <address>").append(lead.getAddress()).append("</address>\n");
-            xml.append(" <zip>").append(lead.getZip()).append("</zip>\n");
-            xml.append(" <city>").append(lead.getCity()).append("</city>\n");
-            xml.append(" <contact>").append(lead.getContact()).append("</contact>\n");
-            xml.append(" <tele>").append(lead.getTele()).append("</tele>\n");
-            xml.append(" <size>").append(lead.getSize()).append("</size>\n");
-            xml.append(" <current_provider>").append(lead.getCurrentProvider()).append("</current_provider>\n");
-            xml.append(" <email>").append(lead.getEmail()).append("</email>\n");
-            xml.append("</lead>\n");
+            // Använd namespace för lead-taggen
+            xml.append("  <syn:lead id=\"").append(lead.getId()).append("\">\n");
+            // Använd namespace för alla element
+            xml.append("    <syn:name>").append(lead.getName()).append("</syn:name>\n");
+            xml.append("    <syn:address>").append(lead.getAddress()).append("</syn:address>\n");
+            xml.append("    <syn:zip>").append(lead.getZip()).append("</syn:zip>\n");
+            xml.append("    <syn:city>").append(lead.getCity()).append("</syn:city>\n");
+            xml.append("    <syn:contact>").append(lead.getContact()).append("</syn:contact>\n");
+            xml.append("    <syn:tele>").append(lead.getTele()).append("</syn:tele>\n");
+            xml.append("    <syn:size>").append(lead.getSize()).append("</syn:size>\n");
+            xml.append("    <syn:current_provider>").append(lead.getCurrentProvider()).append("</syn:current_provider>\n");
+            xml.append("    <syn:email>").append(lead.getEmail()).append("</syn:email>\n");
+            xml.append("  </syn:lead>\n");
         }
 
         xml.append("</leads>");
